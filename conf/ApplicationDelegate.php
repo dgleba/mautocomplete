1e06bd423ebf071295abe0c57504eb191e2fbf77
unknown <oclarke@st-gps-coop465.stackpole.ca>
2014-03-07 16:27:00 -0500

    Removed the module for now, just to ease the testing process. Currently working on getting the autocomplete source working (it's having trouble loading from the database, when you use an array in the js directly it works fine)

---
+++ conf/ApplicationDelegate.php
@@ -0,0 +1,6 @@
+<?php
+class conf_ApplicationDelegate {
+    function block__custom_javascripts() {
+        echo '<script src="js/tagTest.js" type="text/javascript" language="javascript"></script>';
+    }
+}
