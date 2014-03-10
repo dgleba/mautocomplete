7851248dfffd758b854485a0db5d43e939088fdf
unknown <oclarke@st-gps-coop465.stackpole.ca>
2014-03-10 09:19:15 -0400

    Moved the code to a module folder. The widget is functioning as intended now. All the user has to do is add the tags.php and configphp.dbc from the app/actions folder, to their app folder and change the details within. The module does the rest.

--- conf/ApplicationDelegate.php
+++
@@ -1,6 +0,0 @@
-<?php
-class conf_ApplicationDelegate {
-    function block__custom_javascripts() {
-        echo '<script src="js/tagTest.js" type="text/javascript" language="javascript"></script>';
-    }
-}
