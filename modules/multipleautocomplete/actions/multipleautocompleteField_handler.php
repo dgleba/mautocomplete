13e7eff8c1d3d97c8d3e641c7aac6111aeaf96f0
unknown <oclarke@st-gps-coop465.stackpole.ca>
2014-03-11 15:18:45 -0400

    Made it so the user can specify the name of the table the tags will be taken from in the 'multipleautocomplete.txt' file

--- modules/multipleautocomplete/actions/multipleautocompleteField_handler.php
+++ modules/multipleautocomplete/actions/multipleautocompleteField_handler.php
@@ -1,11 +1,25 @@
 <?php
 $field_name = $_POST['fname'];
 $table_name = $_POST['tname'];
+$table_exist = false;
+
+$given = parse_ini_file('../../../tables/'. $table_name .'/multipleautocomplete.txt');
+
+foreach ($given as $key => $value)
+{
+  if ($key == $field_name)
+  {
+    $table_name = $value;
+    $table_exist = true;
+  }
+}
 
 $file = 'multipleautocompleteField.tmp';
 
 $handle = fopen($file, "w");
-fwrite($handle, '1 = '. $table_name . '_tags');
+fwrite($handle, '1 = '. $table_name);
+if(!$table_exist)
+  fwrite($handle, '_tags');
 fwrite($handle, PHP_EOL);
 fwrite($handle, '2 = '. $field_name);
 fclose($handle);
