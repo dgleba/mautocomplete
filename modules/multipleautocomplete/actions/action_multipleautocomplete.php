193e96150caa8322213b09f8f9a6b91162b0ddd6
unknown <oclarke@st-gps-coop465.stackpole.ca>
2014-03-12 09:05:12 -0400

    Got rid of the tmp file, using sessions.

--- modules/multipleautocomplete/actions/action_multipleautocomplete.php
+++ modules/multipleautocomplete/actions/action_multipleautocomplete.php
@@ -1,19 +1,20 @@
 <?php
+session_start();
+$multipleautocomplete_field_name = $_SESSION['field_name'];
+$multipleautocomplete_table_name = $_SESSION['table_name'];
+
 require_once "../../../configphp.dbc";
 $conn = mysql_connect($dbhost, $dbuser, $dbpass) or die('Error connecting to mysql');
 mysql_select_db($dbname);
 $return_arr = array();
-$field = parse_ini_file("multipleautocompleteField.tmp");
-$table_name = $field[1];
-$field_name = $field[2];
 
 if ($conn) {
-    $fetch = mysql_query("SELECT * FROM ". $table_name ." WHERE ". $field_name ." like '%" . mysql_real_escape_string($_GET['term']) . "%'");
+    $fetch = mysql_query("SELECT * FROM ". $multipleautocomplete_table_name ." WHERE ". $multipleautocomplete_field_name ." like '%" . mysql_real_escape_string($_GET['term']) . "%'");
 
     while ($row = mysql_fetch_array($fetch, MYSQL_ASSOC)) {
         $row_array['id'] = $row['id'];
 		
-		$row_array['value'] = $row[$field_name];
+		$row_array['value'] = $row[$multipleautocomplete_field_name];
 
         array_push($return_arr, $row_array);
     }
