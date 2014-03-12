db819d92a7d99cc1d89b4c61cc45ce8d5e5e871e
unknown <oclarke@st-gps-coop465.stackpole.ca>
2014-03-12 09:05:12 -0400

    Got rid of the tmp file, using sessions.

--- modules/multipleautocomplete/actions/multipleautocompleteField_handler.php
+++ modules/multipleautocomplete/actions/multipleautocompleteField_handler.php
@@ -1,26 +1,24 @@
 <?php
-$field_name = $_POST['fname'];
-$table_name = $_POST['tname'];
+session_start();
+
+$multipleautocomplete_field_name = $_POST['fname'];
+$multipleautocomplete_table_name = $_POST['tname'];
 $table_exist = false;
 
-$given = parse_ini_file('../../../tables/'. $table_name .'/multipleautocomplete.txt');
+$given = parse_ini_file('../../../tables/'. $multipleautocomplete_table_name .'/multipleautocomplete.txt');
 
 foreach ($given as $key => $value)
 {
-  if ($key == $field_name)
+  if ($key == $multipleautocomplete_field_name)
   {
-    $table_name = $value;
+    $multipleautocomplete_table_name = $value;
     $table_exist = true;
   }
 }
 
-$file = 'multipleautocompleteField.tmp';
-
-$handle = fopen($file, "w");
-fwrite($handle, '1 = '. $table_name);
 if(!$table_exist)
-  fwrite($handle, '_tags');
-fwrite($handle, PHP_EOL);
-fwrite($handle, '2 = '. $field_name);
-fclose($handle);
+  $multipleautocomplete_table_name = $multipleautocomplete_table_name.'_tags';
+  
+$_SESSION['field_name'] = $multipleautocomplete_field_name;
+$_SESSION['table_name'] = $multipleautocomplete_table_name;
 ?>
