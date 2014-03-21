f45fc72e076dc270f8d67ee727f25d1df4aa5d4e
oclarke <oclarke@st-gps-coop465.stackpole.ca>
2014-03-21 13:45:04 -0400

    Made the widget able to load valuelists with 'order' and 'where' in (it ignores them)

--- modules/multipleautocomplete/actions/multipleautocompleteField_handler.php
+++ modules/multipleautocomplete/actions/multipleautocompleteField_handler.php
@@ -5,31 +5,52 @@ $site_path = $_COOKIE['app_path'];
 
 if(file_exists($site_path."/tables/". $multipleautocomplete_table_name ."/valuelists.ini"))
 {
-  
   $table_exist = false;
   $file_array = file($site_path."/tables/". $multipleautocomplete_table_name ."/valuelists.ini");
-  
-  $column_table_php_index = 1;
-  $valueindex = 1;
+  $tempstr;
+  $clean_array = array();
   
   foreach ($file_array as $line_num => $line)
   {
-    if (strpos ($line, "SELECT"))
+    if(stripos($line, "where") !== false)
+	{
+	  $tempstr = stristr($line, "where", true);
+	}
+	elseif(stripos($line, "order") !== false)
+	{
+	  $tempstr = stristr($line, "order", true);
+	}
+	else
+	{
+	  $tempstr = $line;
+	}
+	$clean_array[$line_num] = $tempstr;
+  }
+  
+  $column_table_php_index = 1;
+  $valueindex = 1;
+  $index;
+  
+  foreach ($clean_array as $value)
+  {
+    if (stripos($value, "select") !== false)
     {
-      $column_table_php = explode("SELECT", $line);
+      $column_table_php = explode("SELECT", $value);
       $column_table_php_list[$column_table_php_index] = $column_table_php[1];
-  	$column_table_php_index = $column_table_php_index + 1;
+  	  $column_table_php_index = $column_table_php_index + 1;
     }
   }
   
-  foreach ($column_table_php_list as $key => $value)
+  foreach ($column_table_php_list as $value)
   {
-    if (strpos ($value, "FROM"))
+    if (stripos($value, "from") !== false)
     {
       $column_table = explode("FROM", $value);
       $columnlist[$valueindex] = trim($column_table[0]);
+      if ($multipleautocomplete_field_name == trim($column_table[0]))
+	    $index = $valueindex;
       $table_roughlist[$valueindex] = $column_table[1];
-  	$valueindex = $valueindex + 1;
+  	  $valueindex = $valueindex + 1;
     }
   }
   
@@ -37,7 +58,6 @@ if(file_exists($site_path."/tables/". $multipleautocomplete_table_name ."/valuel
   {
     $table_templist = explode('"', $value);
     $tablelist[$key] = trim($table_templist[0]);
-    $index = $key;
   }
   
   $multipleautocomplete_field_name = $columnlist[$index];
