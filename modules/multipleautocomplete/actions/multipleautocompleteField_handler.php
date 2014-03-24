c490fd96354daf0b8ad0c873a437e74af7f9c40b
oclarke <oclarke@st-gps-coop465.stackpole.ca>
2014-03-24 13:35:06 -0400

    Added vocabulary functionality (as in yui_autocomplete). Mautocomplete reads from fields.ini to find the vocabulary link, then reads that vocabulary from valuelists.ini

--- modules/multipleautocomplete/actions/multipleautocompleteField_handler.php
+++ modules/multipleautocomplete/actions/multipleautocompleteField_handler.php
@@ -1,73 +1,81 @@
 <?php
-$multipleautocomplete_field_name = $_POST['fname'];
+$multipleautocomplete_field_name = "[".$_POST['fname']."]";
 $multipleautocomplete_table_name = $_POST['tname'];
 $site_path = $_COOKIE['app_path'];
 
-if(file_exists($site_path."/tables/". $multipleautocomplete_table_name ."/valuelists.ini"))
+$field_check = false;
+
+$fields_array = file($site_path . "/tables/". $multipleautocomplete_table_name ."/fields.ini");
+
+foreach($fields_array as $line)
 {
-  $table_exist = false;
-  $file_array = file($site_path."/tables/". $multipleautocomplete_table_name ."/valuelists.ini");
-  $tempstr;
-  $clean_array = array();
-  
-  foreach ($file_array as $line_num => $line)
+  if (trim($line) == $multipleautocomplete_field_name)
+    $field_check = true;
+  if(stripos($line, "vocabulary") !== false && $field_check)
   {
-    if(stripos($line, "where") !== false)
-	{
-	  $tempstr = stristr($line, "where", true);
-	}
-	elseif(stripos($line, "order") !== false)
-	{
-	  $tempstr = stristr($line, "order", true);
-	}
-	else
-	{
-	  $tempstr = $line;
-	}
-	$clean_array[$line_num] = $tempstr;
+    $field_check = false;
+	$vocab_line = explode("=", $line);
+	$multipleautocomplete_field_name = "[" . trim($vocab_line[1]) . "]";
+	break;
   }
-  
-  $column_table_php_index = 1;
-  $valueindex = 1;
-  $index;
-  
-  foreach ($clean_array as $value)
-  {
-    if (stripos($value, "select") !== false)
-    {
-      $column_table_php = explode("SELECT", $value);
-      $column_table_php_list[$column_table_php_index] = $column_table_php[1];
-  	  $column_table_php_index = $column_table_php_index + 1;
-    }
-  }
-  
-  foreach ($column_table_php_list as $value)
-  {
-    if (stripos($value, "from") !== false)
-    {
-      $column_table = explode("FROM", $value);
-      $columnlist[$valueindex] = trim($column_table[0]);
-      if ($multipleautocomplete_field_name == trim($column_table[0]))
-	    $index = $valueindex;
-      $table_roughlist[$valueindex] = $column_table[1];
-  	  $valueindex = $valueindex + 1;
-    }
-  }
-  
-  foreach ($table_roughlist as $key => $value)
-  {
-    $table_templist = explode('"', $value);
-    $tablelist[$key] = trim($table_templist[0]);
-  }
-  
-  $multipleautocomplete_field_name = $columnlist[$index];
-  $multipleautocomplete_table_name = $tablelist[$index];
-  
 }
-else
+
+$file_array = file($site_path . "/tables/" . $multipleautocomplete_table_name . "/valuelists.ini");
+
+//isolate the sql command
+foreach($file_array as $line)
 {
-  $multipleautocomplete_table_name = $multipleautocomplete_table_name.'_tags';
+  if (trim($line) == $multipleautocomplete_field_name)
+    $field_check = true;
+  if(stripos($line, "sql") !== false && $field_check)
+  {
+    $field_check = false;
+	$vocab_line = explode("=", $line);
+	$column_table_full = trim($vocab_line[1]);
+	break;
+  }
 }
+
+//get rid of the quotations
+$rough_column_table_less = explode('"', $column_table_full);
+$column_table_less = trim($rough_column_table_less[1]);
+
+//get rid of where and order since we only want the column and table names
+if (stripos($column_table_less, "where") !== false)
+{
+  if (strpos($column_table_less, "WHERE") !== false)
+    $rough_where_column_table = explode('WHERE', $column_table_less);
+  elseif (strpos($column_table_less, "where") !== false)
+    $rough_where_column_table = explode('where', $column_table_less);
+  $column_table_less = trim($rough_where_column_table[0]);
+}
+
+if (stripos($column_table_less, "order") !== false)
+{
+  if (strpos($column_table_less, "ORDER") !== false)
+    $rough_order_column_table = explode('ORDER', $column_table_less);
+  if (strpos($column_table_less, "order") !== false)
+    $rough_order_column_table = explode('order', $column_table_less);
+  $column_table_less = trim($rough_order_column_table[0]);
+}
+
+//get rid of select, now we have 'column FROM table'
+if (strpos($column_table_less, "SELECT") !== false)
+  $rough_column_table = explode('SELECT', $column_table_less);
+elseif (strpos($column_table_less, "select") !== false)
+  $rough_column_table = explode('select', $column_table_less);
+$column_table = trim($rough_column_table[1]);
+
+//get rid of FROM and retrieve the column and table names
+if (strpos($column_table, "FROM") !== false)
+  $rough_column_table_split = explode('FROM', $column_table);
+elseif (strpos($column_table, "from") !== false)
+  $rough_column_table_split = explode('from', $column_table);
+
+//save the names to variables
+$multipleautocomplete_field_name = trim($rough_column_table_split[0]);
+$multipleautocomplete_table_name = trim($rough_column_table_split[1]);
+
 
 setcookie("field_name", $multipleautocomplete_field_name);
 setcookie("table_name", $multipleautocomplete_table_name);
